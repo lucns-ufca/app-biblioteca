62cd6918534218bcf4fb89366d5b6c2036694030
Lucas <lucas.nascimento@aluno.ufca.edu.br>
2024-09-04 18:58:30 -0300

    Icones modificados e versao minima do android alterada para o 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.equipe17.biblioteca"
-        minSdk = 30
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
